cee75663428687adc3e77fc40ac86807bcbcaf89
saul <luckly083@gmail.com>
2024-05-20 20:48:23 -0500

    deploy

--- config/settings/base.py
+++ config/settings/base.py
@@ -138,14 +138,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "/static/"
+# STATIC_URL = "/static/"
 # STATIC_ROOT = env.str("STATIC_ROOT_PATH")
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static",
 # ]
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = env.str("STATIC_ROOT_PATH")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_ROOT = env.str("MEDIA_ROOT_PATH")
 MEDIA_URL = "/media/"
